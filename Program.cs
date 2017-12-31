5d63aebcf46be7e3982ce9eb3370027f5e54b990
Marina-Reinke <marinareinke8@.com>
2017-12-31 17:07:49 -0200

    inicializado projeto c#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculos_de_Tear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************Calculos de Tear****************");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //aqui eu leio a linha, para a janela ficar aberta
+            Console.ReadLine();
+            
+            Console.WriteLine("By: MarinaReinke.");
+        }
+    }
+}
